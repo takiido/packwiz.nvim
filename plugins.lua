e8f3f3830b77333276d7f2ccd9660aca6fee5e27
takiido <takiid0@proton.me>
2025-08-04 21:10:14 -0500

    format plugin now separate function

--- plugins.lua
+++ plugins.lua
@@ -5,31 +5,26 @@ function M.get_plugins(active_only)
     local result = {}
 
     for _, plugin in ipairs(raw_data) do
+        local plugin_str = format_plugin_str(plugin)
         if active_only then
             if plugin.active then
-                table.insert(
-                    result,
-                    string.format(
-                        "   ■ %s  -  [%s]",
-                        plugin.spec.name or "unknown",
-                        plugin.spec.src
-                    )
-                )
+                table.insert(result, plugin_str)
             end
         else
-            table.insert(
-                result,
-                string.format(
-                    "   %s %s  -  [%s]",
-                    plugin.active and "■" or "□",
-                    plugin.spec.name or "unknown",
-                    plugin.spec.src
-                )
-            )
+            table.insert(result, plugin_str)
         end
     end
 
     return result
+end
+
+function format_plugin_str(plugin)
+    return string.format(
+        "   %s %s  -  [%s]",
+        plugin.active and "■" or "□",
+        plugin.spec.name or "unknown",
+        plugin.spec.src
+    )
 end
 
 return M
